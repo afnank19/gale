7929afc1e7d5ad7dd8a9bfe9a85beb6eff8e5e2d
Afnan Khalid <afnank19@gmail.com>
2025-05-27 00:45:44 +0500

    fix some parser bugs, add duration parsing

--- args.go
+++ args.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func ParseArgs() {
@@ -14,41 +16,82 @@ func ParseArgs() {
 	args := allArgs[1:]
 	fmt.Println("What we need -> ", args)
 
-	for i, argStr := range args {
-		fmt.Println(argStr, i)
-		arg, value := SplitArg(argStr)
-		if value == -1 { // means that argument does not have a value
+	for _, argStr := range args {
+		fmt.Println(argStr)
+		arg, value := splitArg(argStr)
+		if value == "" { // means that argument does not have a value
 			// handleNormalArg()
 			fmt.Println("normal arg detected: ", arg)
 		}
 
 		if arg == "--worker" || arg == "-w" {
+			value, err := strconv.Atoi(value)
+			if err != nil {
+				panic("Arg value should be an integer")
+			}
 			fmt.Println("Value -> ", value)
+			continue
 		}
 		if arg == "--concurrency" || arg == "-c" {
+			value, err := strconv.Atoi(value)
+			if err != nil {
+				panic("Arg value should be an integer")
+			}
 			fmt.Println("Value -> ", value)
+			continue
 		}
 		if arg == "--duration" || arg == "-d" {
 			fmt.Println("Value -> ", value)
+			parseDurationValue(value)
+			continue
 		}
 	}
 }
 
-func SplitArg(arg string) (string, int) {
+func splitArg(arg string) (string, string) {
 	argTokens := strings.Split(arg, "=")
 
 	if len(argTokens) < 2 {
-		return arg, -1
+		return arg, ""
 	}
 
 	if len(argTokens) > 2 {
 		panic("Bad argument provided")
 	}
 
-	value, err := strconv.Atoi(argTokens[1])
-	if err != nil {
-		panic("Bro, value of args can only be ints bro what is wrong with you")
+	return argTokens[0], argTokens[1]
+}
+
+func parseDurationValue(val string) time.Duration {
+	if len(val) < 1 {
+		panic("No value provided for duration")
 	}
 
-	return argTokens[0], value
+	lastChar := val[len(val)-1:]
+
+	numVal := val[:len(val)-1]
+	value, err := strconv.Atoi(numVal)
+	if err != nil {
+		log.Fatalln("ERROR: Arg value should be an integer")
+	}
+
+	if value > 999999 {
+		value = 999999
+	}
+
+	var duration time.Duration
+	if lastChar == "s" {
+		duration += time.Duration(value) * time.Second
+	}
+
+	if lastChar == "m" {
+		duration += time.Duration(value) * time.Minute
+	}
+
+	if lastChar == "h" {
+		duration += time.Duration(value) * time.Hour
+	}
+
+	fmt.Println("Final Duration: ", duration)
+	return duration
 }
--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type State struct {
+	workers     int
+	concurrency int
+	duration    time.Duration
+}
 
 func main() {
 	fmt.Println("GALE! Hammer your server")
 
-	ParseArgs()
+	// ParseArgs()
+	timer1 := time.NewTimer(2 * time.Second)
+
+	// The `<-timer1.C` blocks on the timer's channel `C`
+	// until it sends a value indicating that the timer
+	// fired.
+	fmt.Println("Timer 1 fired", <-timer1.C)
 }
